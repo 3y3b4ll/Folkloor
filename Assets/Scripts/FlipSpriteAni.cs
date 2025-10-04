e923c80afdbe235e3f3eb670a4fa314ce6952361
neti-cyber <anette.jaaniso@gmail.com>
2025-10-04 16:43:32 +0300

    cursor
    player fix ja custom cursor

--- Assets/Scripts/FlipSpriteAni.cs
+++ Assets/Scripts/FlipSpriteAni.cs
@@ -2,22 +2,27 @@ using UnityEngine;
 
 public class FlipSpriteAni : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    [Tooltip("Horizontal input axis name")]
-    public string horizontalAxis = "Horizontal";
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Camera worldCamera; // assign Main Camera in inspector
 
     void Awake()
     {
-        if (spriteRenderer == null)
+        if (!spriteRenderer) 
             spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        if (!worldCamera)
+            worldCamera = Camera.main;
     }
 
     void Update()
     {
-        float h = Input.GetAxis(horizontalAxis);
-        if (h > 0.01f)
-            spriteRenderer.flipX = false;
-        else if (h < -0.01f)
-            spriteRenderer.flipX = true;
+        // Convert mouse position to world space
+        Vector3 mouseWorld = worldCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        // If the mouse is right of player, face right; left of player, face left
+        if (mouseWorld.x > transform.position.x)
+            spriteRenderer.flipX = true;   // facing right (flipped)
+        else if (mouseWorld.x < transform.position.x)
+            spriteRenderer.flipX = false;  // facing left (not flipped)
     }
 }
